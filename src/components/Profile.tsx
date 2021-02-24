a1303ae4447fc7e83b60b283820972a7ab6a43fa
Wanderson Chaves <wandersonscpibr@gmail.com>
2021-02-24 17:11:42 -0300

    add profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://github.com/wandersonchaves.png"
+        alt="Wanderson Chaves"
+      />
+      <div>
+        <strong>Wanderson Chaves</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
